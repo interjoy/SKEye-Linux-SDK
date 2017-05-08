38b85f59b4ccf99f54e371e186cdd945e13cdfa8
zfguo <zfguo@interjoy.com.cn>
2017-05-08 11:44:52 +0800

    complete function

--- SKEyeSDKDemo/main.cpp
+++ SKEyeSDKDemo/main.cpp
@@ -23,7 +23,7 @@ bool ReadBmp(char *filename, unsigned char *data)
 //Â·¾¶»Øµ÷
 void SKEyeSDK_ImagePath_CallBackFunction(char *PATH, char *service_name)
 {
-	SKEyeSDK_ImagePath_CallBack(PATH, service_name, _JsonData);
+	SKEyeSDK_ImagePath(PATH, service_name, _JsonData);
 }
 //reb
 void SKEyeSDK_Image_Function(int With, int Height, char *service_name)
@@ -48,7 +48,7 @@ void SKEyeSDK_Image_CallBackFunction(int With, int Height, char *service_name)
 		printf("Open is error\n");
 		return;
 	}
-	SKEyeSDK_Image_CallBack(data, With, Height, service_name, _JsonData);
+	SKEyeSDK_Image(data, With, Height, service_name, _JsonData);
 }
 unsigned char *ReadData(char *File, int &Datalen)
 {
@@ -70,29 +70,23 @@ unsigned char *ReadData(char *File, int &Datalen)
 void SKEyeSDK_ImagePath_Function(char *PATH, char *service_name)
 { 
 	char  *JsonData ;
-	//memset(JsonData, 0, 1024 * 10);
 	JsonData=SKEyeSDK_ImagePath(PATH, service_name);
 	printf("%s\n", JsonData);
-	//delete[] JsonData;
 }
 
 int main()
 {
-
-
-
 	char Api_Key[] = "466da4221010f834191e2da500b4a23b";
 	char Api_Secret[] = "ddc8bd498e7c29f929862ab7391a7b89";
 	char Image_Url[] = "http://pic.58pic.com/58pic/12/92/83/39j58PIChF6.jpg";
-	char service_name[] = "objects";
-	
-	char PATH[1024];
-	memset(PATH,0,1024);
-	strcpy(PATH,"object3.jpg");
+	char service_name_ob[] = "objects";
+	char service_name_fr[] ="fruits";
+	char PATH[1024]="object3.jpg";
 	SKEyeSDK_Init(Api_Key, Api_Secret);
-	SKEyeSDK_ImagePath_Function(Image_Url, service_name);
-	SKEyeSDK_ImagePath_CallBackFunction(Image_Url, service_name);
-	SKEyeSDK_Image_Function(640, 480, service_name);
-	SKEyeSDK_Image_CallBackFunction(640, 480, service_name);
+	SKEyeSDK_ImagePath_Function(Image_Url, service_name_fr);
+	SKEyeSDK_ImagePath_CallBackFunction(PATH, service_name_ob);
+	SKEyeSDK_Image_Function(640, 480, service_name_ob);
+	SKEyeSDK_Image_CallBackFunction(640, 480, service_name_ob);
+	getchar();
 	return 0;
 }
